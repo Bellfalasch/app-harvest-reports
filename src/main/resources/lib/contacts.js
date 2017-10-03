ff867e43301a6736c11da49aef1ac6d9b37315e8
Bellfalasch <bellfalasch@gmail.com>
2017-10-03 13:55:36 +0200

    Note to self

--- src/main/resources/lib/contacts.js
+++ src/main/resources/lib/contacts.js
@@ -4,6 +4,7 @@ var libs = {
 	harvestApi: require('/lib/harvest-api')
 };
 
+// We need to wrap functionality into services to be able to control access better since services exposes URL endpoints by default.
 exports.all = function() {
 	libs.harvestApi.request({
 		'endpoint': 'contacts'
