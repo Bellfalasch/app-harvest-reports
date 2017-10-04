c207d404b4aa9785ea979f62677f8324c62e4e60
Bellfalasch <bellfalasch@gmail.com>
2017-10-04 10:56:10 +0200

    Output week data (reports will be based on this)

--- src/main/resources/admin/tools/harvest/harvest.js
+++ src/main/resources/admin/tools/harvest/harvest.js
@@ -1,23 +1,34 @@
 var libs = {
 	portal: require('/lib/xp/portal'),
 	thymeleaf: require('/lib/xp/thymeleaf'),
-	moment: require("/lib/moment-timezone")
-}
+	moment: require("/lib/moment")
+};
 
 var timestamp = Date.now();
+var view = resolve('harvest.html');
 
 exports.get = function(req) {
-    var view = resolve('harvest.html');
 
     // Variables required by the Launcher Panel
     var adminUrl = libs.portal.url({path: '/admin'})
     var adminAssetsUrl = libs.portal.url({path: "/admin/assets/" + timestamp});
     var assetsUrl = libs.portal.assetUrl({path: ""});
+
+	// Initiate moment() and get this weeks number
+	libs.moment().format();
+	log.info( libs.moment().isoWeek() );
+	var weekNow = libs.moment().isoWeek();
+	var weekLast = weekNow--;
+
     var params = {
         adminUrl: adminUrl,
         assetsUrl: assetsUrl,
 		appId: 'harvest',
-        appName: 'Harvest Report Tool'
+        appName: 'Harvest Report Tool',
+		moment: {
+			weekNow: weekNow.toString(),
+			weekLast: weekLast.toString()
+		}
     };
 
     return {
